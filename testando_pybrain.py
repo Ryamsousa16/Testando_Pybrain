15d2637831ba2895340fee06fcdda9ec835734fa
Ryam Sousa <Ryam.sousa04@gmail.com>
2021-05-06 22:13:40 -0300

    Analisando e testando o algoritmo

--- testando_pybrain.py
+++ testando_pybrain.py
@@ -15,3 +15,24 @@ nn = buildNetwork(2, 16, 1,bias =True)
 #treinando o algoritmo
 trainer = BackpropTrainer(nn, ds)
 
+#analisando a evolução do algoritmo
+for i in range(2000):
+    print(trainer.train())
+
+#com o buildNetwork utilizando 4 neurônios, temos 0.013496550372475475 de margem de erro
+#com o buildNetwork utilizando 16 neurônios, temos 0.0009338957668739359 de margem de erro
+#teste 1 com 512 neurônios: 1.6434602192104412e-32 de margem de erro
+#teste 2 com 512 neurônios: 1.3702349577667055e-30 de margem de erro
+#irei utilizar neste algoritmo apenas 16 neurônios
+
+#alguns testes por curiosidade
+while True:
+    sleep = float(input("How many hours did you sleep? "))
+    study = float(input("How many hours did you study? "))
+
+    z = nn.activate((sleep, study))[0] * 10.0
+
+    if sleep >= 1.1 or study >= 1.1:
+        break
+
+    print("{:.2f}".format(z))
